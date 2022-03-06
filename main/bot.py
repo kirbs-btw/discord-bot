489c730129a9aad078c76119aebf6c764faedc9f
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-03-06 15:38:20 +0100

    Add files via upload

--- main/bot.py
+++ main/bot.py
@@ -2,25 +2,33 @@ import discord
 import sqlite3
 import datetime
 
+print("starting bot...")
 client = discord.Client()
 
 
 @client.event
 async def on_ready():
-    print(f"hello i´m {client.user.name}")
+    now = datetime.datetime.now().strftime("%H:%M:%S")
+
+    print(f"Bot online:     {client.user.name}")
+    print(f"Online at:      {now}\n")
     client.loop.create_task(status_task())
+
 async def status_task():
     while True:
-        await client.change_presence(activity=discord.Game('mit marlin'), status=discord.Status.online)
+        await client.change_presence(activity=discord.Game('kw-corp.de'), status=discord.Status.online)
 
 
 @client.event
 async def on_message(message):
+
     if message.author.bot:
+
         return
 
-    streak_print = 0
     if message.content == '%daily':
+        streak_print = 0
+
         conn = sqlite3.connect("db.sql")
         cur = conn.cursor()
         command = "CREATE TABLE IF NOT EXISTS daily(discord_id INT, daily_streak INT, last_daily DATE)"
@@ -40,6 +48,7 @@ async def on_message(message):
 
         command = f"SELECT * FROM daily WHERE discord_id = '{discordId}'"
         userData = cur.execute(command).fetchall()
+
         if userData == []:
             userNotExist = True
         if userNotExist:
@@ -63,6 +72,51 @@ async def on_message(message):
             command = f"UPDATE daily SET daily_streak = '{streak}', last_daily = '{today}' WHERE discord_id = '{discordId}'"
             cur.execute(command)
             conn.commit()
-    await message.channel.send(f"daily streak = [{streak_print}]")
+            conn.close()
+
+        info = addMoney(discordId, streak_print, str(lastDaily))
+
+        logMsg = f"{message.author.name} --- %daily       current money = {info[0]} --- money added = {info[1]} --- id = {discordId}"
+        print(logMsg)
+        if info[1] == 0:
+            await message.channel.send("You can´t %daily twice!")
+
+        await message.channel.send(f"Current balance = {info[0]} : {message.author.name}")
+        await message.channel.send(f"daily streak = {streak_print}")
+
+def addMoney(discordId, streak, lastDaily):
+    today = datetime.date.today()
+
+    newMoney = 0
+    moneyAdd = 0
+
+    conn = sqlite3.connect("db.sql")
+    cur = conn.cursor()
+
+    command = f"SELECT * FROM userData WHERE id = '{discordId}'"
+    userRow = cur.execute(command).fetchall()
+
+    streakValue = streak * 100
+
+    if userRow == []:
+        moneyAdd = streakValue
+        command = f"INSERT INTO userData VALUES({discordId}, {moneyAdd})"
+        cur.execute(command)
+        conn.commit()
+        newMoney = moneyAdd
+    elif str(lastDaily) == str(today) and userRow != []:
+        moneyAdd = 0
+        newMoney = userRow[0][1]
+    else:
+        currentMoney = userRow[0][1]
+
+        moneyAdd = streakValue
+        newMoney = currentMoney + streakValue
+        command = f"UPDATE userData SET money = '{newMoney}' WHERE id = '{discordId}'"
+        cur.execute(command)
+        conn.commit()
+
+    return [newMoney, moneyAdd]
+
 
 client.run('')
